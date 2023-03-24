c60e9bb94940f23e9b9e675e92145af1d59e4541
aldoalfians <aldoboetd908@gmail.com>
2023-03-24 16:12:43 +0700

    slicing home screen

--- lib/screens/main/screens/home/home_screen.dart
+++ lib/screens/main/screens/home/home_screen.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:my_nikah_booking/screens/main/screens/home/widgets/home_adzan_card.dart';
+import 'package:my_nikah_booking/utils/constant.dart';
+import 'package:my_nikah_booking/utils/extension/context_extension.dart';
 import 'package:my_nikah_booking/widgets/scaffold_background.dart';
 import 'package:textless/textless.dart';
 
@@ -35,8 +38,110 @@ class HomeScreen extends StatelessWidget {
             )
           ],
         ),
-        body: Center(
-          child: Text("Beranda"),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const HomeAdzanCard(),
+                const SizedBox(height: 24),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.43,
+                      decoration: BoxDecoration(
+                        color: context.cardColor,
+                        borderRadius: const BorderRadius.all(
+                          Radius.circular(16),
+                        ),
+                      ),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.symmetric(
+                                vertical: 16, horizontal: 8),
+                            child: CircleAvatar(
+                              radius: 28,
+                              backgroundColor: context.secondaryColor,
+                              child: Image.asset(
+                                ImagePath.pinIllustration,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ),
+                          Padding(
+                            padding:
+                                const EdgeInsets.only(left: 16, bottom: 16),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                "Booking".text.bold.size(12),
+                                "Nikah di KUA"
+                                    .text
+                                    .bold
+                                    .size(16)
+                                    .color(context.primaryColor),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(16),
+                      ),
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.43,
+                        decoration: BoxDecoration(
+                          color: context.cardColor,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(16),
+                          ),
+                        ),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 16, horizontal: 8),
+                              child: CircleAvatar(
+                                radius: 28,
+                                backgroundColor: context.secondaryColor,
+                                child: Image.asset(
+                                  ImagePath.routeIllustration,
+                                  fit: BoxFit.contain,
+                                ),
+                              ),
+                            ),
+                            Padding(
+                              padding:
+                                  const EdgeInsets.only(left: 16, bottom: 16),
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  "Booking".text.bold.size(12),
+                                  "Nikah diluar KUA"
+                                      .text
+                                      .bold
+                                      .size(16)
+                                      .color(context.primaryColor),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
